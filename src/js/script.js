97694a3cf70b08e7c689c29a25b3eaee447d9487
Felipe <fecerazi@gmail.com>
2025-04-01 09:10:11 -0300

    declaração e variáveis

---
+++ src/js/script.js
@@ -0,0 +1,21 @@
+//Declarações e variáveis
+var nome ="fiap";
+console.log(nome);
+
+let idade = 18;
+console.log(idade);
+
+const sobrenome ="cidade";
+console.log(sobrenome);
+//undefined
+let aula;
+console.log(aula);
+//null
+let valor = null;
+console.log(valor);
+
+let exemplo1={};
+
+let exemplo2=[];
+
+
